0060598dd7873e54bfa4ecb8ef646bac987cf7c3
Shindarev Nikita <nshindarev@gmail.com>
2021-11-01 14:53:56 +0300

    Added label with new fonts

--- my-test-chat/my-test-chat/Classes/ViewController.swift
+++ my-test-chat/my-test-chat/Classes/ViewController.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIAlertViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIAlertViewDelegate{
 
     // MARK: UIFields
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var viewPhoto: UIView!
-    @IBOutlet weak var btnSave: UIButton!
+    @IBOutlet weak var profileView: UIView!
     @IBOutlet weak var btnEdit: UIButton!
+    @IBOutlet weak var btnSave: UIButton!
     
     var imagePicker = UIImagePickerController()
+    
+    
+    // MARK: ================== LIFECYCLE EVENTS =======================
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        profileView.layer.cornerRadius = profileView.bounds.width / 2
+        imageView.layer.cornerRadius = profileView.bounds.width / 2
+        
+        btnSave.layer.cornerRadius = 14
+        imagePicker.delegate = self
+    }
+    
+    // MARK: ================== UI EVENTS =======================
     
     @IBAction func onEditTapped(_ sender: UIButton) {
 
@@ -23,26 +37,59 @@ class ViewController: UIViewController, UIAlertViewDelegate, UIImagePickerContro
 
         let alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
         alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: { _ in
-            print("camera")
-            self.imagePicker.sourceType = UIImagePickerController.SourceType.camera
-            self.present(self.imagePicker, animated: true, completion: nil)
+            self.switchImagePicker(.camera)
         }))
 
         alert.addAction(UIAlertAction(title: "Gallery", style: .default, handler: { _ in
-            print("gallery")
-            self.imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
-            self.present(self.imagePicker, animated: true, completion: nil)
+            self.switchImagePicker(.photoLibrary)
         }))
         
 
         alert.addAction(UIAlertAction.init(title: "Cancel", style: .cancel, handler: nil))
         present(alert, animated: true, completion: nil)
 
-        viewPhoto.addSubview(imagePicker.view)
     }
-    
-    //MARK:- ***************  UIImagePickerController delegate Methods ****************
 
+    // MARK: ================== SUPPORT METHODS =======================
+    
+    func switchImagePicker (_ type: UIImagePickerController.SourceType){
+        switch type{
+        case .camera:
+            if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.camera))
+            {
+                self.imagePicker.sourceType = UIImagePickerController.SourceType.camera
+                self.imagePicker.allowsEditing = true
+            }
+            else
+            {
+                let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        case .photoLibrary:
+            if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerController.SourceType.photoLibrary))
+            {
+                self.imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+                self.imagePicker.allowsEditing = true
+            }
+            else
+            {
+                let alert  = UIAlertController(title: "Warning", message: "You don't have photo library access", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+        default:
+            return
+        }
+        
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+}
+
+
+// MARK: ================== UIImagePickerController DELEGATE METHODS =======================
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
 
         // The info dictionary may contain multiple representations of the image. You want to use the original.
@@ -52,31 +99,8 @@ class ViewController: UIViewController, UIAlertViewDelegate, UIImagePickerContro
 
         // Set photoImageView to display the selected image.
         imageView.image = selectedImage
-        viewPhoto.isHidden = true
 
         // Dismiss the picker.
         dismiss(animated: true, completion: nil)
-    }
-
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        viewPhoto.layer.cornerRadius = viewPhoto.bounds.width / 2
-        btnSave.layer.cornerRadius = 14
-        
-        imagePicker.delegate = self
-    }
-    
-
-    
-}
-
-extension ViewController {
-    func alertMessageOk(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.actionSheet)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
     }
 }
